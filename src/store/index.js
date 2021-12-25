7c67c2c3aded340f04a6fd72f53b3d5d41094133
AssassinFlash <857558227@qq.com>
2021-12-25 23:08:47 +0800

    2021-12-25-first commit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+// vue2.0 new Vuex.store({}) 创建仓库
+// vue3   createStore({})
+export default createStore({
+  state: {
+    username: 'zs'
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
